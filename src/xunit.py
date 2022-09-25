8777d6e875c388e27c56e03546fb34119b230012
ac2393921 <ac2393921@gmail.com>
2022-09-25 23:55:08 +0900

    add: TestCase class and division some function of WasRun

--- src/xunit.py
+++ src/xunit.py
@@ -1,21 +1,25 @@
 from typing import Optional
 
 
-class WasRun:
+class TestCase:
     def __init__(self, name: str) -> None:
-        self.was_run: Optional[bool] = None
         self.name: str = name
 
     def run(self) -> None:
-        self.test_method()
         method = getattr(self, self.name)
         method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.was_run: Optional[bool] = None
 
     def test_method(self) -> None:
         self.was_run = True
 
 
-test = WasRun("testMethod")
+test = WasRun("test_method")
 print(test.was_run)
 test.run()
 print(test.was_run)
