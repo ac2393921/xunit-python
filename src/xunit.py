ca03177857db96f4b87775bcc8a8fe8e4cf96783
ac2393921 <ac2393921@gmail.com>
2022-09-26 22:13:19 +0900

    add: set up fucntion

--- src/xunit.py
+++ src/xunit.py
@@ -5,26 +5,35 @@ class TestCase:
     def __init__(self, name: str) -> None:
         self.name: str = name
 
+    def set_up(self):
+        pass
+
     def run(self) -> None:
         method = getattr(self, self.name)
         method()
 
 
 class TestCaseTest(TestCase):
+    def set_up(self):
+        self.test = WasRun("test_method")
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert not test.was_run
-        test.run()
-        assert test.was_run
+        self.test.run()
+        assert self.test.was_run
+
+    def test_set_up(self):
+        self.test.run()
+        assert self.test.was_set_up
 
 
 class WasRun(TestCase):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
+    def set_up(self):
         self.was_run: Optional[bool] = None
+        self.was_set_up = 1
 
     def test_method(self) -> None:
         self.was_run = True
 
 
 TestCaseTest("test_running").run()
+TestCaseTest("test_set_up").run()
